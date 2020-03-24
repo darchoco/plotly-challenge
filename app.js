e9460ef6f546d4a7bff7419eab0c512b2437e130
darchoco <56795928+darchoco@users.noreply.github.com>
2020-03-23 20:15:51 -0700

    pushing comments, adjustment for dictionary creation

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
+
+//defining intial variable for reference in function optionChanged
 var demoInfo =  d3.select("#sample-metadata"); 
 
-
-function createDictionary(dictionary,key,value)
-{
-  dictionary[key] = value;
-}
-  
-
 //filter function defined to be called when data is generated
-//utilizes startingdata variable that is defined before the filter is calle
+//utilizes data received from json string and input from dropdown to compare, and return only where there is a match
 function filterData(data, input)
 {
 for (var i = 0; i < data.length; i++)
@@ -21,9 +16,13 @@ for (var i = 0; i < data.length; i++)
 }
 }
 
+
+//initial data pull
 function getdata()
 {
-d3.json("samples.json").then((data) => {
+  //sets up json read
+d3.json("samples.json").then((data) => 
+{
   //dyanmically obtains the dropdown data from the json file and inserts it into the dropdown
  var dropdown = d3.select("#selDataset").selectAll("select");
     dropdown.data(data.names)
@@ -33,27 +32,32 @@ d3.json("samples.json").then((data) => {
     {
       return `<option value = "${d}">${d}</option>`
     });
-    var intialid = d3.select("#selDataset").node().value
-    //printing data
-    optionChanged(intialid)
-
-
+    //finds id that is currently selected in the dropdown
+    var currentID = d3.select("#selDataset").node().value
+    optionChanged(currentID)
   });
 
 };
 
+//set up function called in index.html on change, as well as from intial pull up
 function optionChanged(input)
 {
-  console.log(input)
- // var filtereddata = d3.select("#selDataset").node().value;
+  //call json data for info from change
   d3.json("samples.json").then((data) => {
-    console.log(data)
     
-      //calling defined function to filter metadat
+      //creating empty data dictionary that will be used to put together the OTU ID and counts
+      //declaring two arrays for displaying the top ten OTU ID counts to be filled later
       var dataDict = {}
+      topTenOTUIDs = []
+      topTenOTUValues = []
+      //call filter function to filter down to metadata
       var metadataPlotData = filterData(data.metadata, input);
+
+      //call filter function to filter down to samples data
       var samplesPlotData = filterData(data.samples, input);
+      //clear any data in the Demoggraphic Info window
       demoInfo.html("")
+      //write in each section of the data by calling filtered data
         pLine = demoInfo.append("p");
         pLine.append('p').text(`id: ${metadataPlotData.id}`);
         pLine.append('p').text(`ethnicity: ${metadataPlotData.ethnicity}`);
@@ -63,12 +67,14 @@ function optionChanged(input)
         pLine.append('p').text(`bbtype: ${metadataPlotData.bbtype}`);
         pLine.append('p').text(`wfreq: ${metadataPlotData.wfreq}`);
 
+        //loop through otu_id and value data to combine them together for proper sorting
+        //also assign the proper naming for displaying on the graph
         for (var i = 0; i < samplesPlotData.otu_ids.length; i++)
         {
-            createDictionary(dataDict, `OTU ${samplesPlotData.otu_ids[i]}`,samplesPlotData.sample_values[i]);
+            dataDict[`OTU ${samplesPlotData.otu_ids[i]}`] = samplesPlotData.sample_values[i];
               
         }
-        // Create items array
+        // Create items array in order to sort dictionary
         var items = Object.keys(dataDict).map(function(key) {
           return [key, dataDict[key]];
         });
@@ -76,10 +82,10 @@ function optionChanged(input)
         items.sort(function(first, second) {
           return second[1] - first[1];
         });
+        //slice to only hold the top 10 highest IDs
         topTenOTU = items.slice(0, 10);
-        // Create a new array with only the first 5 items
-        topTenOTUIDs = []
-        topTenOTUValues = []
+
+        //loop through sliced array from dictionary, push to arrays for displaying
         for (var i = 0; i < topTenOTU.length; i++)
         {
           topTenOTUIDs.push(topTenOTU[i][0])
@@ -90,6 +96,7 @@ function optionChanged(input)
       {
         x: samplesPlotData.otu_ids,
         y: samplesPlotData.sample_values,
+        text: samplesPlotData.otu_labels,
         mode: 'markers',
         marker: {
           size: samplesPlotData.sample_values,
@@ -105,7 +112,7 @@ function optionChanged(input)
       {
         xaxis: { title: "OTU ID" }
       };
-    
+     //  Create the Traces
       var trace2 =
       {
         type: 'bar',
@@ -121,9 +128,9 @@ function optionChanged(input)
         hovertext: samplesPlotData.otu_labels,
         maxdisplayed:10
       };
-      // Plot the chart to a div tag with id "plot"
+      // Plot the charts to the divs bubble, bar
       Plotly.newPlot("bubble", data1, layout1);
       Plotly.newPlot("bar",data2, layout2)
 })};
-
+//pull intial data on load
 getdata()
